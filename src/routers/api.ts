8cce78d058b58437edf481f1997c2b14a0ca772e
roland.pakai <roland.pakai@lightware.com>
2025-03-01 15:58:12 +0100

    feat: implemented temperaturesRouter

--- src/routers/api.ts
+++ src/routers/api.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import temperaturesRouter from './temperaturesRouter';
 
 const api = express.Router();
+
+api.use('/temperatures', temperaturesRouter);
 
 export default api;
---
+++ src/routers/temperaturesRouter.ts
@@ -0,0 +1,31 @@
+import { Router, Request, Response } from 'express';
+import { WeatherSchema } from '../schemas/validationSchemas';
+
+const router = Router();
+const weatherAPIBaseUrl = 'http://api.weatherapi.com/v1/current.json';
+
+router.get('/', async (req: Request, res: Response) => {
+  const location = (req.query?.location ? req.query.location : 'Budapest') as string;
+  const params = new URLSearchParams();
+
+  params.append('key', process.env.WEATHER_API_KEY!);
+  params.append('q', location);
+
+  try {
+    const apiResp = await fetch(`${weatherAPIBaseUrl}?${params.toString()}`)
+    const weatherData = await apiResp.json();
+    const validationResult = WeatherSchema.safeParse(weatherData);
+
+    if (validationResult.success) {
+      const typedWeatherData = validationResult.data;
+      res.status(200).json({ location, temperature: typedWeatherData.current.temp_c });
+    } else {
+      console.error("Invalid weather data:", validationResult.error.errors);
+      res.status(400).json({ error: "Invalid weather data" });
+    }
+  } catch (error: unknown) {
+    res.status(500).json({ error });
+  }
+});
+
+export default router;
